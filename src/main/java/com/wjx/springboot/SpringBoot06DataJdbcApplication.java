08426c3cf27d1d5640c8397cd10e7c055e98c64d
fengchuidaoguxiang <33154249+fengchuidaoguxiang@users.noreply.github.com>
2019-06-21 21:09:24 +0800

    spring-boot和mybatis整合

---
+++ src/main/java/com/wjx/springboot/SpringBoot06DataJdbcApplication.java
@@ -0,0 +1,14 @@
+package com.wjx.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@SpringBootApplication
+public class SpringBoot06DataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06DataJdbcApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/wjx/springboot/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.wjx.springboot.controller;
+
+
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String,Object> map(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from department");
+
+        return list.get(0);
+    }
+
+}
